2b9cf1e4848dd9144e06d3984702123f96e76d4f
Yousheng <ermachenkovvova@gmail.com>
2022-02-22 12:47:41 +0300

    add tests

--- src/payment/payment.service.spec.ts
+++ src/payment/payment.service.spec.ts
@@ -1,18 +1,74 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PaymentService } from './payment.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {MailerModule, MailerService} from "@nest-modules/mailer";
+import {ConfigModule, ConfigService} from "nestjs-config";
+import * as path from "path";
+import {Payment} from "./entities/payment.entity";
+import {TransactionService} from "../transaction/transaction.service";
+import {StoresService} from "../stores/stores.service";
+import {HttpService} from "@nestjs/axios";
+import {PaymentService} from "./payment.service";
+import {Transaction} from "../transaction/entities/transaction.entity";
+import {Stores} from "../stores/entities/stores.entity";
 
-describe('PaymentService', () => {
-  let service: PaymentService;
+const dotEnvPath = path.resolve(__dirname, '..', '.env');
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentService],
-    }).compile();
+describe('PaymentService',() => {
+    let service: PaymentService;
+    let mailerService: MailerService;
 
-    service = module.get<PaymentService>(PaymentService);
-  });
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.load(
+                    path.resolve(__dirname, 'config', '**!(*.d).config.{ts,js}'),
+                    {
+                        path: dotEnvPath,
+                    },
+                ), //ci
+                TypeOrmModule.forRootAsync({
+                    useFactory: (config: ConfigService) => {
+                        return {
+                            ...config.get('../config/database.config.ts'),
+                            entities: [path.join(__dirname,'**', '*.entity.{ts,js}')],
+                            keepConnectionAlive: true
+                        };
+                    },
+                    inject: [ConfigService],
+                }),
+                TypeOrmModule.forFeature([Payment, Transaction, Stores]),
+                MailerModule,
+            ],
+            providers: [
+                PaymentService, TransactionService, StoresService,
+                {
+                    provide: MailerService,
+                    useValue: {
+                        get: jest.fn(async () => {
+                        }),
+                        // really it can be anything, but the closer to your actual logic the better
+                    }
+                },
+                {
+                    provide: HttpService,
+                    useValue: {
+                        get: jest.fn(async () => {
+                        }),
+                        // really it can be anything, but the closer to your actual logic the better
+                    }
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<PaymentService>(PaymentService);
+        mailerService = module.get<MailerService>(MailerService);
+
+        mailerService.sendMail = jest.fn();
+
+    });
+    it('should ', function () {
+        expect("").toBeDefined();
+    });
+
+
 });
